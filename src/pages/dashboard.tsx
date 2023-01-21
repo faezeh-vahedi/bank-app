4a5160492f1f9c539b79af3f5e774f9113a5b1f1
faezeh-vahedi <118221831+faezeh-vahedi@users.noreply.github.com>
2023-01-21 20:29:16 +0330

    add tailwindcss to project

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,9 +2,9 @@
 const Home = () => {
     return (
         <>
-            <main>
-                sdfsalkdflkasdf
-            </main>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         </>
     )
 
